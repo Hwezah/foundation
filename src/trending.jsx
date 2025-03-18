c490979c5a1c59cf96a82a29c53e7e46b48bf4b0
Hwezah <hwezah.dev@gmail.com>
2025-03-18 15:41:01 +0300

    minor local storage tweeks

--- src/trending.jsx
+++ src/trending.jsx
@@ -9,7 +9,13 @@ export default function Trending() {
   useEffect(() => {
     const storedTrends = localStorage.getItem("trends");
     if (storedTrends) {
-      setTrends(JSON.parse(storedTrends)); // Convert string back to array
+      try {
+        setTrends(JSON.parse(storedTrends)); // Attempt to parse and update the state
+      } catch (error) {
+        console.error("Error parsing trends from localStorage:", error);
+        // Optionally, handle the error (e.g., reset trends to empty array)
+        setTrends([]);
+      }
     }
   }, []);
 
@@ -60,11 +66,62 @@ export default function Trending() {
               video.id.videoId || video.id.channelId || video.id.playlistId;
             return videoId ? (
               <li key={videoId} className="mb-4 cursor-pointer relative flex">
-                <img
+                <iframe
+                  width="100%"
+                  height="200"
+                  src={`https://www.youtube.com/embed/${videoId}`}
+                  title={video.snippet.title}
+                  className="rounded-lg shadow-lg"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                  allowFullScreen
+                ></iframe>
+                {/* <img
                   src={video.snippet.thumbnails.medium.url}
                   alt={video.snippet.title}
                   className="rounded-lg shadow-lg"
-                />
+                /> */}
+                <div className="absolute top-[50%] left-[50%] translate-[-50%] flex items-center justify-center rounded-lg">
+                  <svg
+                    version="1.1"
+                    id="Capa_1"
+                    xmlns="http://www.w3.org/2000/svg"
+                    xmlns:xlink="http://www.w3.org/1999/xlink"
+                    width="151px"
+                    height="151px"
+                    viewBox="-10.89 -10.89 384.80 384.80"
+                    xml:space="preserve"
+                    fill="#fff"
+                    stroke="#fff"
+                    stroke-width="7.2605"
+                  >
+                    <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                    <g
+                      id="SVGRepo_tracerCarrier"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    ></g>
+                    <g id="SVGRepo_iconCarrier">
+                      {" "}
+                      <g>
+                        {" "}
+                        <g>
+                          {" "}
+                          <path
+                            style="fill:#fff;"
+                            d="M155.859,241.706c-3.605,0-7.188-0.863-10.37-2.499c-7.682-3.899-12.459-11.699-12.459-20.339 v-74.717c0-8.631,4.777-16.431,12.479-20.353c7.582-3.823,16.991-3.062,23.795,1.898l51.308,37.355 c5.878,4.286,9.389,11.182,9.389,18.455s-3.511,14.175-9.389,18.457l-51.308,37.362 C165.382,240.19,160.73,241.706,155.859,241.706z M155.868,133.032c-1.747,0-3.497,0.415-5.055,1.207 c-3.742,1.907-6.072,5.709-6.072,9.912v74.723c0,4.208,2.33,8.005,6.078,9.909c3.666,1.881,8.246,1.515,11.587-0.915 l51.311-37.367c2.859-2.081,4.574-5.443,4.574-8.988c0-3.543-1.715-6.902-4.574-8.995l-51.311-37.355 C160.496,133.77,158.238,133.032,155.868,133.032z"
+                          ></path>{" "}
+                        </g>{" "}
+                        <g>
+                          {" "}
+                          <path
+                            style="fill:#fff;"
+                            d="M181.512,363.024C81.427,363.024,0,281.601,0,181.513C0,81.43,81.427,0,181.512,0 c100.089,0,181.513,81.43,181.513,181.513C363.025,281.601,281.601,363.024,181.512,363.024z M181.512,11.71 C87.88,11.71,11.71,87.883,11.71,181.513c0,93.627,76.17,169.802,169.802,169.802c93.627,0,169.803-76.175,169.803-169.802 C351.315,87.883,275.139,11.71,181.512,11.71z"
+                          ></path>{" "}
+                        </g>{" "}
+                      </g>{" "}
+                    </g>
+                  </svg>
+                </div>
                 <h3 className="font-semibold absolute bottom-0 left-0 w-full p-2 text-sm z-10">
                   {video.snippet.title.split(" ").slice(0, 4).join(" ")}
                   {video.snippet.title.split(" ").length > 4 ? "..." : ""}
