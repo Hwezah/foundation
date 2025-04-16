de1fabeda79118947ea73ec6148686ff26f60ccb
Hwezah <hwezah.dev@gmail.com>
2025-04-16 22:12:36 +0300

    tried adding animation for podcast titles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,21 @@
 export default {
   content: [
     "./index.html",
-    "./src/*.{js,jsx,ts,tsx}", // Adjust if needed
+    "./src/**/*.{js,jsx,ts,tsx}", // Make sure this matches your folder structure
   ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        marquee: {
+          "0%": { transform: "translateX(100%)" },
+          "100%": { transform: "translateX(-100%)" },
+        },
+      },
+      animation: {
+        marquee: "marquee 10s linear infinite",
+        "marquee-hover": "marquee 10s linear infinite paused", // optional for hover effect
+      },
+    },
   },
   plugins: [],
 };
